7768107869660fd7f9f6a9d2a11cb06705b87018
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-12-08 18:13:39 -0500

    package example part1 ✌️ 🎺

---
+++ temp/Jacksonville.java
@@ -0,0 +1,28 @@
+package temp;
+import temp.*;
+/* File name : MammalInt.java */
+
+public class Jacksonville {
+
+  public static void main(String args[]) {
+     Jacksonville m = new Jacksonville();
+     Rain q = new Rain(); 
+
+     q.eat();
+     q.travel();
+  }
+
+   public void eat() {
+      System.out.println("Mammal eats");
+   }
+
+   public void travel() {
+      System.out.println("Mammal travels");
+   }
+
+   public int noOfLegs() {
+      return 0;
+   }
+
+
+}
---
+++ temp/Rain.java
@@ -0,0 +1,11 @@
+package temp;
+public class Rain{
+  public Rain(){
+  }
+  public void travel(){
+    System.out.println("stupid");
+  }
+  public void eat(){
+    System.out.println("rain");
+  }
+}
